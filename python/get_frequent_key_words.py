6e44a062b6c67f843c5bad1960a58560bcc1d94c
Rostislav Yavorskiy <ryavorsky@gmail.com>
2017-05-23 12:47:30 +0300

    TF-IDF

---
+++ python/get_frequent_key_words.py
@@ -0,0 +1,123 @@
+# First, use mystem.exe -cd --format json titles_and_abstracts.txt titles_and_abstracts.json
+import json
+import math
+import itertools
+
+
+# Configuration
+stopwords_for_all = "../data/stopwords_for_all.txt"
+path = "../data/Newtonew/"
+input_file_name = "titles_and_abstracts.json"
+output_file_name = "frequent_words.txt"
+central_words = ["урок", "ученик", "родитель", "знание", "технология"]
+N = 560
+
+
+# get normalized word from json
+def lemma(d):
+    if "analysis" not in d:
+        return ""
+    else:
+        analysis = d["analysis"]
+        if len (analysis) == 0 :
+            return ""
+        else:
+            return analysis[0]["lex"]
+
+
+def increment(d, v):
+    if v in d:
+        d[v] += 1
+    else:
+        d[v] = 1
+
+
+def count_frequency(list_of_values):
+    res = dict()
+    for v in list_of_values:
+        increment(res, v)
+    return res
+
+
+# get data from json file
+def extract_lists_of_words_from_json(words_to_remove=[]):
+    res = []  # will be list of [list of words] for each paper
+
+    f = open(path + input_file_name, "r", encoding="utf-8")
+    lines = f.readlines()
+
+    # extract set of normalized words for each paper
+    current_line = 0
+    while current_line < len(lines):
+        title = lines[current_line+1]
+        abstract = lines[current_line+2]
+        current_line += 4
+
+        title_words = [lemma(e) for e in json.loads(title)]
+        abstract_words = [lemma(e) for e in json.loads(abstract)]
+        words_list = title_words + abstract_words
+        clean_words_list = []
+        for w in words_list:
+            if (len(w) > 2) and (w not in words_to_remove):
+                clean_words_list.append(w)
+
+        res.append(clean_words_list)
+
+    return res
+
+
+def tf_idf(list_of_texts):
+    all_words = dict()    # for each word the number of documents it appears in
+    for text in list_of_texts:
+        for w in set(text):
+            increment(all_words, w)
+
+    res = []
+    for text in list_of_texts:
+        tf_idf_values = [[word, tf * math.log2(N/all_words[word])] for word, tf in count_frequency(text).items()]    # frequency of each word in this text
+        tf_idf_values.sort(key=lambda x: x[1], reverse=True)
+        print(tf_idf_values)
+        res.append(tf_idf_values)
+
+    return  res
+
+
+# The main part
+def main():
+    papers_data = extract_lists_of_words_from_json([])  # array of [list of words]
+
+    key_words = []
+    for words in tf_idf(papers_data):
+        key_words = key_words + [w[0] for w in words[:60]]
+    key_words = [[w, stat] for w,stat in count_frequency(key_words).items()]
+    key_words.sort(key=lambda x: x[1], reverse=True)
+    print(key_words)
+
+
+    # Calculate statistics for each pair
+    word_pairs = dict()
+    set_of_frequent_words = set([e[0] for e in []])
+    for word_list in papers_data:
+        word_set = set_of_frequent_words.intersection(word_list)
+        for pair in itertools.combinations(word_set, 2):
+            if pair in word_pairs:
+                word_pairs[pair] += 1
+            else:
+                word_pairs[pair] = 1
+
+    # Find nearest
+    for central_word in set_of_frequent_words:
+        nearest = []
+        for pair in word_pairs:
+            if central_word in pair:
+                w = list(set(pair).difference({central_word}))[0]
+                #print("урок", pair, word_pairs[pair])
+                nearest.append([w, word_pairs[pair]])
+                nearest.sort(key=lambda x: x[1], reverse=True)
+        print(central_word, nearest[:5])
+
+    print(len(papers_data), " papers analyzed")
+
+
+
+main()
